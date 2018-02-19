5280f6647a9c841565126aed76a76cc3fac737af
Ankush <Ankush@Ankush-PC>
2018-02-19 14:33:35 +0530

    New changes

--- TrueBotanicals-Staging/src/org/UIInterface/LoginPage.java
+++ TrueBotanicals-Staging/src/org/UIInterface/LoginPage.java
@@ -32,6 +32,25 @@ public class LoginPage {
 	@FindBy(how=How.XPATH, using=".//button[@class='close-button' and @data-close-popup='newsletter_sign_up']")
 	public WebElement crossNL;
 	
+	@FindBy(how=How.XPATH, using=".//*[@class='title-container']//h4")
+	public WebElement MainHead;
+	
+	@FindBy(how=How.ID, using="first_name_r")
+	public WebElement Fname;
+	
+	@FindBy(how=How.ID, using="last_name_r")
+	public WebElement Lname;
+	
+	@FindBy(how=How.ID, using="email_r")
+	public WebElement Email;
+	
+	@FindBy(how=How.ID, using="password_r")
+	public WebElement Pword;
+	
+	@FindBy(how=How.XPATH, using=".//div[@id='password']//input[@type='button']")
+	public WebElement Submit;
+	
+	
 	
 	public void ClickSignUp()
 	{
@@ -41,9 +60,20 @@ public class LoginPage {
 		{
 			wait.until(ExpectedConditions.elementToBeClickable(LoginOption.get(i)));
 			crossNL.click();
+			System.out.println(LoginOption.get(i).getText());
 			if(LoginOption.get(i).getText().equalsIgnoreCase("Sign Up"))
 			{
 				LoginOption.get(i).click();
+				try {
+					wait.until(ExpectedConditions.textToBePresentInElement(MainHead, "I’M A NEW CUSTOMER."));
+														
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.out.println(e.getMessage());
+				}
+				
+				
+				
 				break;
 			}
 			else {
